d1adfbbade281764510ac3b8672c64f2a9fb479e
Alessio Stalla <alessiostalla@gmail.com>
2022-05-09 16:46:31 +0200

    "Pylasu" initial version

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,6 +4,10 @@ from pylasu.model import Node, Position, Point
 
 
 class ModelTest(unittest.TestCase):
+    def test_empty_node(self):
+        node = Node()
+        self.assertIsNone(node.origin)
+
     def test_node_with_position(self):
         node = Node(specified_position=Position(Point(1, 0), Point(2, 1)))
         self.assertIsNotNone(node.origin)
