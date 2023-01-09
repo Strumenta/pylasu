1199ec015f66ce41ff165bca932dc0466c25c7e0
Alessio Stalla <alessiostalla@gmail.com>
2023-01-09 14:28:13 +0100

    Port another transformer test from Kolasu

--- tests/transformation/test_parse_tree_to_ast_transformers.py
+++ tests/transformation/test_parse_tree_to_ast_transformers.py
@@ -5,7 +5,7 @@ from typing import List
 from antlr4 import CommonTokenStream, InputStream
 
 from pylasu.mapping.parse_tree_to_ast_transformer import ParseTreeToASTTransformer
-from pylasu.model import Node, Named
+from pylasu.model import Node, Named, ReferenceByName
 from pylasu.transformation.transformation import PropertyRef
 from tests.antlr_entity.AntlrEntityLexer import AntlrEntityLexer
 from tests.antlr_entity.AntlrEntityParser import AntlrEntityParser
@@ -18,15 +18,38 @@ class EModule(Node, Named):
 
 @dataclass
 class EEntity(Node, Named):
-    features: List = field(default_factory=list)
+    features: List["EFeature"] = field(default_factory=list)
+
+
+@dataclass
+class EFeature(Node, Named):
+    type: "EType" = None
+
+
+class EType(Node):
+    pass
+
+
+@dataclass
+class EBooleanType(EType):
+    pass
+
+
+@dataclass
+class EStringType(EType):
+    pass
+
+
+@dataclass
+class EEntityRefType(EType):
+    entity: ReferenceByName[EEntity]
 
 
 class ParseTreeToASTTransformerTest(unittest.TestCase):
 
     def test_simple_entities_transformer(self):
         transformer = ParseTreeToASTTransformer(allow_generic_node=False)
-        transformer.register_node_factory(
-            AntlrEntityParser.ModuleContext, lambda ctx: EModule(name=ctx.name.text))\
+        transformer.register_node_factory(AntlrEntityParser.ModuleContext, lambda ctx: EModule(name=ctx.name.text))\
             .with_child(AntlrEntityParser.ModuleContext.entity, PropertyRef("entities"))
         transformer.register_node_factory(AntlrEntityParser.EntityContext, lambda ctx: EEntity(name=ctx.name.text))
         expected_ast = EModule("M", [EEntity("FOO", []), EEntity("BAR", [])])
@@ -36,6 +59,47 @@ module M {
    entity BAR { }
 }
         """))
+        self.assertEqual(expected_ast, actual_ast)
+
+    def test_entities_with_features_transformer(self):
+        transformer = ParseTreeToASTTransformer(allow_generic_node=False)
+        transformer.register_node_factory(AntlrEntityParser.ModuleContext, lambda ctx: EModule(name=ctx.name.text)) \
+            .with_child(AntlrEntityParser.ModuleContext.entity, PropertyRef("entities"))
+        transformer.register_node_factory(AntlrEntityParser.EntityContext, lambda ctx: EEntity(name=ctx.name.text)) \
+            .with_child(AntlrEntityParser.EntityContext.feature, PropertyRef("features"))
+        transformer.register_node_factory(AntlrEntityParser.FeatureContext, lambda ctx: EFeature(name=ctx.name.text))\
+            .with_child(AntlrEntityParser.FeatureContext.type_spec, PropertyRef("type"))
+        transformer.register_node_factory(AntlrEntityParser.Boolean_typeContext, EBooleanType)
+        transformer.register_node_factory(AntlrEntityParser.String_typeContext, EStringType)
+        transformer.register_node_factory(
+            AntlrEntityParser.Entity_typeContext,
+            lambda ctx: EEntityRefType(ReferenceByName(ctx.target.text)))
+
+        expected_ast = EModule(
+            "M",
+            [
+                EEntity(
+                    "FOO",
+                    [EFeature("A", EStringType()), EFeature("B", EBooleanType())]
+                ),
+                EEntity(
+                    "BAR",
+                    [EFeature("C", EEntityRefType(ReferenceByName("FOO")))]
+                )
+            ]
+        )
+        actual_ast = transformer.transform(
+            self.parse_entities("""
+            module M {
+               entity FOO {
+                A: string;
+                B: boolean;
+                 
+               }
+               entity BAR {
+                 C: FOO;
+               }
+            }"""))
         self.assertEqual(expected_ast, actual_ast)
 
     def parse_entities(self, code: str) -> AntlrEntityParser.ModuleContext:
