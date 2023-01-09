f26c4d12530332f1940e1066e8ffec7343b64181
Alessio Stalla <alessiostalla@gmail.com>
2023-01-09 14:34:49 +0100

    Mark assert_asts_are_equal as not yet implemented.

--- pylasu/testing/testing.py
+++ pylasu/testing/testing.py
@@ -2,4 +2,4 @@ from pylasu.model import Node
 
 
 def assert_asts_are_equal(expected: Node, actual: Node, context: str = "<root>", consider_position: bool = False):
-    pass
+    raise NotImplementedError("TODO implement this. Transformers tests don't use this yet.")
