6b329aeebea6558a9c127b6e4fe4ba303bcf474e
Alessio Stalla <alessiostalla@gmail.com>
2023-10-10 13:26:12 +0200

    Lint

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -25,10 +25,11 @@ class CW(Node):
 
 class ProcessingTest(unittest.TestCase):
     def test_search_by_type(self):
-        self.assertEquals(["1", "2", "3", "4", "5", "6"],
-                         [i.name for i in box.search_by_type(Item)])
-        self.assertEquals(["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
-                         [n.name for n in box.search_by_type(Node)])
+        self.assertEquals(["1", "2", "3", "4", "5", "6"], [i.name for i in box.search_by_type(Item)])
+        self.assertEquals(
+            ["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
+            [n.name for n in box.search_by_type(Node)]
+        )
 
     def test_replace_in_list(self):
         a1 = AW("1")
--- tests/test_traversing.py
+++ tests/test_traversing.py
@@ -9,8 +9,10 @@ class TraversingTest(unittest.TestCase):
         self.assertEquals([], [n.name for n in box.walk_within(pos(15, 1, 15, 1))])
 
     def test_walk_within_with_root_position(self):
-        self.assertEquals(["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
-                         [n.name for n in box.walk_within(box.position)])
+        self.assertEquals(
+            ["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
+            [n.name for n in box.walk_within(box.position)]
+        )
 
     def test_walk_within_with_leaf_position(self):
         self.assertEquals(["6"], [n.name for n in box.walk_within(pos(13, 3, 13, 9))])
@@ -19,19 +21,24 @@ class TraversingTest(unittest.TestCase):
         self.assertEquals(["small", "3", "4", "5"], [n.name for n in box.walk_within(pos(7, 5, 11, 5))])
 
     def test_walk_depth_first(self):
-        self.assertEquals(["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
-                         [n.name for n in box.walk()])
+        self.assertEquals(
+            ["root", "first", "1", "2", "big", "small", "3", "4", "5", "6"],
+            [n.name for n in box.walk()]
+        )
 
     def test_walk_leaves_first(self):
-        self.assertEquals(["1", "first", "2", "3", "4", "5", "small", "big", "6", "root"],
-                         [n.name for n in box.walk_leaves_first()])
+        self.assertEquals(
+            ["1", "first", "2", "3", "4", "5", "small", "big", "6", "root"],
+            [n.name for n in box.walk_leaves_first()]
+        )
 
     def test_walk_descendants(self):
-        self.assertEquals(["first", "1", "2", "big", "small", "3", "4", "5", "6"],
-                         [n.name for n in box.walk_descendants()])
+        self.assertEquals(
+            ["first", "1", "2", "big", "small", "3", "4", "5", "6"],
+            [n.name for n in box.walk_descendants()]
+        )
 
     def test_walk_ancestors(self):
         box.assign_parents()
         item4 = box.contents[2].contents[0].contents[1]
-        self.assertEquals(["small", "big", "root"],
-                         [n.name for n in item4.walk_ancestors()])
+        self.assertEquals(["small", "big", "root"], [n.name for n in item4.walk_ancestors()])
