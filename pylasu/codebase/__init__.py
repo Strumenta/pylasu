967036f685f03b112243ea15fcc24d786a25fb07
ftomassetti <federico@strumenta.com>
2025-03-22 17:30:39 +0100

    Minor

--- pylasu/codebase/__init__.py
+++ pylasu/codebase/__init__.py
@@ -1,5 +1,5 @@
-from codebase_file import CodebaseFile
-from lwlanguage import CODEBASE_LANGUAGE, CODEBASE_FILE
-from serialization import register_codebase_deserializers
+from .codebase_file import CodebaseFile
+from .lwlanguage import CODEBASE_LANGUAGE, CODEBASE_FILE
+from .serialization import register_codebase_deserializers
 
 __all__ = ["CodebaseFile", "CODEBASE_LANGUAGE", "CODEBASE_FILE", "register_codebase_deserializers"]
